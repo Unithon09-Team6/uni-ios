07bec132377ec0d8ca954f2a67e03e357be9ce12
Daehee Kim <0824dh@naver.com>
2022-09-03 13:45:03 +0900

    feat :: BaseClass

---
+++ iOS/Sources/Base/BaseTC.swift
@@ -0,0 +1,40 @@
+//
+//  BaseTC.swift
+//  UNITHON-Team6-iOS
+//
+//  Created by 김대희 on 2022/09/03.
+//  Copyright © 2022 com.UNITHON-Team6. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class BaseTableViewCell: UITableViewCell {
+    // MARK: - Initializing
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.configureUI()
+        self.setupConstraints()
+        self.initialize()
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initialize() {
+        
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+    
+}
+
--- iOS/Sources/Base/BaseVC.swift
+++ iOS/Sources/Base/BaseVC.swift
@@ -1,8 +1,69 @@
-//
-//  baseVC.swift
-//  solvedAC
-//
-//  Created by baegteun on 2021/10/29.
-//
-
 import UIKit
+import RxSwift
+import RxCocoa
+import Moya
+import SnapKit
+import Then
+import Kingfisher
+
+class BaseViewController: UIViewController {
+    
+    // 자주 사용하는 프로퍼티를 넣어줘도 되요
+    // MARK: - Property
+    var disposeBag = DisposeBag()
+    private(set) var didSetupConstraints = false
+    
+    // MARK: - Initializing
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - View Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.setLayout()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+          self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = Asset.backgroundColor.color
+        self.configureUI()
+        self.setupConstraints()
+        self.initialize()
+        self.view.setNeedsUpdateConstraints()
+    }
+    
+    override func updateViewConstraints() {
+        if !self.didSetupConstraints {
+            self.setupConstraints()
+            self.didSetupConstraints = true
+        }
+        super.updateViewConstraints()
+    }
+    
+    func initialize() {
+        
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+}
