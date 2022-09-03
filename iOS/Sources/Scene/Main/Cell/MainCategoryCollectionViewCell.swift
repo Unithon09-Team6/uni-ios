8a15c39d0d7e204c36d69975e7f824ff82eb9f05
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-09-03 15:25:39 +0900

    feat :: #1 하단 카테고리 Layout

--- iOS/Sources/Scene/Main/Cell/MainCategoryCollectionViewCell.swift
+++ iOS/Sources/Scene/Main/Cell/MainCategoryCollectionViewCell.swift
@@ -8,8 +8,71 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class MainCategoryCollectionViewCell: UICollectionViewCell {
     
-  
+    private let bgView = UIView().then {
+        $0.cornerRadius = 20
+        $0.backgroundColor = .purple
+    }
     
+    private let descriptionLabel = UILabel().then {
+        $0.textColor = .white
+        $0.text = "description"
+        $0.font = UNITHONTeam6IOSFontFamily.Pretendard.semiBold.font(size: 13)
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.textColor = .white
+        $0.text = "한식"
+        $0.font = UNITHONTeam6IOSFontFamily.Pretendard.bold.font(size: 22)
+    }
+    
+    private let foodImageView = UIImageView().then {
+        $0.backgroundColor = .white
+    }
+    
+    // MARK: - LifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Function
+    
+    func setData(mainCategory: MainCategory) {
+        bgView.backgroundColor = mainCategory.color
+    }
+    
+    private func setLayout() {
+        addSubviews([bgView])
+        bgView.addSubviews([descriptionLabel, titleLabel, foodImageView])
+        
+        bgView.snp.makeConstraints {
+            $0.top.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(28)
+            $0.leading.equalToSuperview().inset(18)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(6)
+            $0.leading.equalToSuperview().inset(18)
+        }
+        
+        foodImageView.snp.makeConstraints {
+            $0.trailing.bottom.equalToSuperview().inset(22)
+            $0.width.height.equalTo(65)
+        }
+    }
 }
