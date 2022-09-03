76b996bcb9b113e56d3916afbfe078ac437bf6fa
Daehee Kim <0824dh@naver.com>
2022-09-03 20:19:20 +0900

    feat :: RecipesDetailViewController.swift

---
+++ iOS/Sources/Scene/RecipesDetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RecipesDetailViewController.swift
+//  UNITHON-Team6-iOS
+//
+//  Created by 김대희 on 2022/09/03.
+//  Copyright © 2022 com.UNITHON-Team6. All rights reserved.
+//
+
+import UIKit
+
+class RecipesDetailViewController: BaseViewController {
+    
+}
