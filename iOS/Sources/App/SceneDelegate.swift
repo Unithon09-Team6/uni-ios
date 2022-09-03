84271bf42d1197fbad9b9b25eaed2290f7e09149
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-09-03 16:06:48 +0900

    feat :: 앱 전체 다크모드 설정

--- iOS/Sources/App/SceneDelegate.swift
+++ iOS/Sources/App/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainViewController = TimerVC()
+        let mainViewController = MainVC()
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
 
